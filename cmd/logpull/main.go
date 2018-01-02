65d1a2dcad2eb7400b7c7d810c1a50a09d530c9e
Jay Mundrawala <jay@thechamberofunderstanding.com>
2018-01-01 18:24:36 -0600

    Some config cleanup

--- cmd/logpull/main.go
+++ cmd/logpull/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
-	"crypto/tls"
-	"io/ioutil"
-
+	"github.com/BurntSushi/toml"
 	logpull "github.com/jaym/logpull/pkg"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "logpull",
+	Short: "logpull is a HTTP file server",
+}
+
+var srvCmd = &cobra.Command{
+	Use:   "serve configFile",
+	Short: "Start the file server",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var conf logpull.ConfigFile
+		_, err := toml.DecodeFile(args[0], &conf)
+		if err != nil {
+			logrus.WithError(err).Fatal("Could not load config")
+		}
+		serverConf, err := conf.ToServerConfig()
+		if err != nil {
+			logrus.WithError(err).Fatal("Could not load config")
+		}
+
+		logpull.Spawn(serverConf)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(srvCmd)
+}
+
 func main() {
-	serverCert, err := tls.LoadX509KeyPair("./example/testserver.crt", "./example/testserver.key")
-	if err != nil {
-		logrus.WithError(err).Fatalf("Could not load cert")
-	}
-
-	clientCaCert, err := ioutil.ReadFile("./example/thechamberofunderstanding.com.crt")
-
-	logpull.Spawn(logpull.ServerConfig{
-		ListenAddress: "localhost:10000",
-		Path:          "./data",
-		ServerCert:    serverCert,
-		ClientCaCert:  clientCaCert,
-	})
+	rootCmd.Execute()
 }
--- pkg/config.go
+++ pkg/config.go
@@ -2,6 +2,8 @@ package logpull
 
 import (
 	"crypto/tls"
+	"fmt"
+	"io/ioutil"
 )
 
 type ServerConfig struct {
@@ -9,4 +11,51 @@ type ServerConfig struct {
 	Path          string
 	ServerCert    tls.Certificate
 	ClientCaCert  []byte
+}
+
+type ConfigFile struct {
+	CertPath string            `toml:"cert"`
+	KeyPath  string            `toml:"key"`
+	Server   *ConfigFileServer `toml:"server"`
+}
+
+type ConfigFileServer struct {
+	ListenAddress    string `toml:"listen_address"`
+	Path             string `toml:"path"`
+	ClientCaCertPath string `toml:"client_ca_cert"`
+}
+
+func (c *ConfigFile) ToServerConfig() (ServerConfig, error) {
+	if c.Server == nil {
+		return ServerConfig{}, fmt.Errorf("Server configuration not provided")
+	}
+
+	if c.Server.ListenAddress == "" {
+		return ServerConfig{}, fmt.Errorf("server.listen_address not provided")
+	}
+
+	if c.Server.Path == "" {
+		return ServerConfig{}, fmt.Errorf("server.path not provided")
+	}
+
+	if c.Server.ClientCaCertPath == "" {
+		return ServerConfig{}, fmt.Errorf("server.path not provided")
+	}
+
+	cert, err := tls.LoadX509KeyPair(c.CertPath, c.KeyPath)
+	if err != nil {
+		return ServerConfig{}, err
+	}
+
+	clientCaCert, err := ioutil.ReadFile(c.Server.ClientCaCertPath)
+	if err != nil {
+		return ServerConfig{}, err
+	}
+
+	return ServerConfig{
+		ListenAddress: c.Server.ListenAddress,
+		Path:          c.Server.Path,
+		ClientCaCert:  clientCaCert,
+		ServerCert:    cert,
+	}, nil
 }
